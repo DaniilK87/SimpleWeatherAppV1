e5172417ec24b7d73e96593631ef7e8a013dddc4
Daniil <denko@list.ru>
2020-08-31 15:26:02 +0500

    Не получается реализовать обработчик нажатия на item в CityFragment

--- app/src/main/java/com/example/simpleweatherappv1/MainActivity.java
+++ app/src/main/java/com/example/simpleweatherappv1/MainActivity.java
@@ -51,7 +51,10 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/example/simpleweatherappv1/ui/cityFragment/CityFragment.java
+++ app/src/main/java/com/example/simpleweatherappv1/ui/cityFragment/CityFragment.java
@@ -1,16 +1,20 @@
 package com.example.simpleweatherappv1.ui.cityFragment;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.simpleweatherappv1.R;
 
@@ -19,18 +23,45 @@ public class CityFragment extends Fragment {
 
     private CityViewModel slideshowViewModel;
 
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        String[] data = getResources().getStringArray(R.array. items );
+
+        
         slideshowViewModel =
                 ViewModelProviders.of(this).get(CityViewModel.class);
         View root = inflater.inflate(R.layout.fragment_city, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
+        final TextView textView = root.findViewById(R.id.searchCity);
         slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+                initRecyclerView(data);
             }
         });
         return root;
+    }
+
+    private void initRecyclerView(String[] data) {
+         RecyclerView recyclerView = getView().findViewById(R.id.recyclerView);
+         recyclerView.setHasFixedSize( true );
+
+        // Менеджер
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        // Адаптер
+        SocnetAdapter adapter = new SocnetAdapter(data);
+        recyclerView.setAdapter(adapter);
+
+        // Установим слушателя
+        adapter.SetOnItemClickListener(new SocnetAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast. makeText (CityFragment.this, String. format ( "%s - %d" ,
+                        ((TextView)view).getText(), position), Toast. LENGTH_SHORT ).show();
+            }
+        });
     }
 }
